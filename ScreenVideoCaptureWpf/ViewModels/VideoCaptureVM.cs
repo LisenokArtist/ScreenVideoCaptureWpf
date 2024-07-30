1c71a8d98837a1f98cc754ee698fdbe723ea4bb0
LisenokArtist <lisenokartist@gmail.com>
2024-07-30 17:56:38 +0300

    Добавлены файлы представления данных

---
+++ ScreenVideoCaptureWpf/ViewModels/VideoCaptureVM.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace ScreenVideoCaptureWpf.ViewModels
+{
+    public class VideoCaptureVM
+    {
+        private Image _imageControl;
+
+        public VideoCaptureVM(Image imageControl)
+        {
+            _imageControl = imageControl;
+        }
+    }
+}
