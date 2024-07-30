08180a3eda5eb75145d0dcd986edebda4d60f589
LisenokArtist <lisenokartist@gmail.com>
2024-07-30 20:39:31 +0300

    Добавлен конвертер описания

---
+++ ScreenVideoCaptureWpf/Core/Converters/DXAdapterDescriptionConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ScreenVideoCaptureWpf.Core.Converters
+{
+    public class DXAdapterDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is SharpDX.DXGI.AdapterDescription desc)
+            {
+                return desc.Description;
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ScreenVideoCaptureWpf/Core/Converters/DXOutputDesctiptionConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ScreenVideoCaptureWpf.Core.Converters
+{
+    public class DXOutputDesctiptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var t = value.GetType();
+            if (value is SharpDX.DXGI.OutputDescription output)
+            {
+                switch (parameter.ToString())
+                {
+                    case "name": return output.DeviceName;
+                    case "resolution": return $"{output.DesktopBounds.Right - output.DesktopBounds.Left}x{output.DesktopBounds.Bottom - output.DesktopBounds.Top} ({output.DesktopBounds.Left}:{output.DesktopBounds.Top})";
+                }
+            }
+
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
