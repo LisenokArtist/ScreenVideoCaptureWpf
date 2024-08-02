fcbeb25cf93c3e24027556199cda589bf48c5690
LisenokArtist <lisenokartist@gmail.com>
2024-08-02 10:51:57 +0300

    Добавлен bool конвертер

---
+++ ScreenVideoCaptureWpf/Core/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ScreenVideoCaptureWpf.Core.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolean) return boolean == true ? Visibility.Visible : Visibility.Collapsed;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility) return visibility == Visibility.Visible ? true : false;
+            return Binding.DoNothing;
+        }
+    }
+
+    public class InverseBooleanToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolean) return boolean == true ? Visibility.Collapsed : Visibility.Visible;
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility visibility) return visibility == Visibility.Visible ? false : true;
+            return Binding.DoNothing;
+        }
+    }
+}
