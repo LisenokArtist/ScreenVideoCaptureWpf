10490cbd7298f54c6ebb20444d152d69f499166b
LisenokArtist <lisenokartist@gmail.com>
2024-08-03 13:25:53 +0300

    Добавлено расшенение для bitmat для object detection

--- ScreenVideoCaptureWpf/Core/Extensions/BitmapExtension.cs
+++ ScreenVideoCaptureWpf/Core/Extensions/BitmapExtension.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using System.Drawing;
 using System.IO;
 using System.Windows.Media.Imaging;
 
@@ -18,6 +21,40 @@ namespace ScreenVideoCaptureWpf.Core.Extensions
             bitmapimage.EndInit();
 
             return bitmapimage;
+        }
+
+        public static System.Drawing.Bitmap ToBitmap<TPixel>(this Image<TPixel> image) where TPixel : unmanaged, IPixel<TPixel>
+        {
+            using var memoryStream = new MemoryStream();
+            var imageEncoder = image.Configuration.ImageFormatsManager.GetEncoder(PngFormat.Instance);
+            image.Save(memoryStream, imageEncoder);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return new System.Drawing.Bitmap(memoryStream);
+        }
+
+        public static System.Drawing.Bitmap Draw(
+            this System.Drawing.Bitmap bitmap, 
+            IEnumerable<YoloDotNet.Models.ObjectDetection> predictions,
+            Font font = null)
+        {
+            font ??= new Font("Tahoma", 8);
+
+            using Graphics graphics = Graphics.FromImage(bitmap);
+            using Pen pen = new Pen(System.Drawing.Color.FromKnownColor(KnownColor.Red), 2);
+
+            foreach (var p in predictions)
+            {
+                graphics.DrawPolygon(pen, new System.Drawing.Point[] {
+                    new System.Drawing.Point(p.BoundingBox.Left, p.BoundingBox.Top),
+                    new System.Drawing.Point(p.BoundingBox.Right, p.BoundingBox.Top),
+                    new System.Drawing.Point(p.BoundingBox.Right, p.BoundingBox.Bottom),
+                    new System.Drawing.Point(p.BoundingBox.Left, p.BoundingBox.Bottom)});
+                graphics.DrawString($"{p.Label.Name} ({p.Confidence})", font, Brushes.Red, new System.Drawing.Point(p.BoundingBox.Left, p.BoundingBox.Top));
+            }
+
+            return bitmap;
         }
     }
 }
